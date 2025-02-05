c527ca50037887ace2f0552dd657c6b95d5fe325
Emilia Perotto <100068661@csisd.org>
2025-02-05 14:58:11 -0600

    gbf

--- Timing.java
+++ Timing.java
@@ -9,29 +9,45 @@ public class Timing {
     int SIZE = 0;
     Scanner in = new Scanner(System.in);
     System.out.println("Input filename: ");
-    String filename = in.next();
+    String filename = in.nextLine();
     Scanner fin = new Scanner(new File(filename));
     int j = 0;
-    while(fin.hasNextLine()){
+    while(fin.hasNextInt()){
       j++;
+      System.out.println("while");
+      fin.nextLine();
     }
+    // System.out.println("after");
+    fin = new Scanner(new File(filename));
     int[] arr1 = new int[j];               //putting text inputs into an array
-    for (int i = 0; i < j - 1; i++) {
+    for (int i = 0; i < j; i++) {
       arr1[i] = fin.nextInt();
-    arr = new int[arr1[i]];
-    for (int h = 0; h < arr1[i]; h++) {
-      arr[h] = rand.nextInt();
-    }  
-    }   //
-    for (int i = 0; i < j - 1; i++) {
-      arr1[i] = fin.nextInt();
-    }
-  
+      // System.out.println("j");
+      arr = new int[arr1[i]];
+      for (int h = 0; h < arr1[i]; h++) {
+        arr[h] = rand.nextInt();
+        // System.out.println("h");
+      } 
     // generate random numbers for array
+    for (int p = 0; p < 10; p++) {
+      // System.out.println("p");
+      //int target = rand.nextInt();
     long start = System.currentTimeMillis();
-    Util.bubbleSort(arr1);
+    Util.selectionSort(arr);
     long end = System.currentTimeMillis();
     long diff = end - start;
-    System.out.println(diff);
+    System.out.println("Size: " + arr1[i] + " trial: " + (p + 1) + " diff: " + diff);
+
+
+    }
+    System.out.println("end");
+    }
+  
+    // // generate random numbers for array
+    // long start = System.currentTimeMillis();
+    // Util.bubbleSort(arr1);
+    // long end = System.currentTimeMillis();
+    // long diff = end - start;
+    // System.out.println(diff);
   }
 }
